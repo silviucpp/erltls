996e5844f94d95731a3dded951b13c4dc572fc99
Silviu Caragea <silviu.caragea@woowteam.com>
2017-01-16 15:47:31 +0200

    Don't hide all missing symbols at linking.

--- c_src/nif.mk
+++ c_src/nif.mk
@@ -30,16 +30,16 @@ UNAME_SYS := $(shell uname -s)
 
 ifeq ($(UNAME_SYS), Darwin)
     CC ?= cc
-	CFLAGS ?= -O3 -std=c99 -arch x86_64 -finline-functions -Wall -Wmissing-prototypes
+	CFLAGS ?= -O3 -std=c99 -arch x86_64 -finline-functions -Wall
 	CXXFLAGS ?= -O3 -arch x86_64  -Wall
-    LDFLAGS ?= -arch x86_64 -flat_namespace -undefined suppress
+    LDFLAGS ?= -arch x86_64
 else ifeq ($(UNAME_SYS),freebsd)
 	CC ?= cc
-	CFLAGS ?= -O3 -std=c99 -finline-functions -Wall -Wmissing-prototypes
+	CFLAGS ?= -O3 -std=c99 -finline-functions -Wall
 	CXXFLAGS ?= -O3 -finline-functions -Wall
 else ifeq ($(UNAME_SYS),linux)
 	CC ?= gcc
-	CFLAGS ?= -O3 -std=c99 -finline-functions -Wall -Wmissing-prototypes
+	CFLAGS ?= -O3 -std=c99 -finline-functions -Wall
 	CXXFLAGS ?= -O3 -finline-functions -Wall
 endif
 
