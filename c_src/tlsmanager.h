2ce6620e235428bb381ccc64ed300c6f444ab03d
Irina Guberman <irina.guberman@xaptum.com>
2018-05-03 12:16:29 -0500

    troubleshoot loading cert from memory

--- c_src/tlsmanager.h
+++ c_src/tlsmanager.h
@@ -30,8 +30,8 @@ struct ContextProperties
     std::string ciphers;
     std::string dh_file;
     std::string ca_certfile;
-    ErlNifBinary cert = NULL;
-    ErlNifBinary key = NULL;
+    ErlNifBinary cert;
+    ErlNifBinary key;
 
     uint32_t reuse_sessions_ttl_sec;
     bool use_session_ticket;
